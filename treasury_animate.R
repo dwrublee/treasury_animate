66ca7cf03c22c4de6bcde0561ea301e63b6c10b7
dwrublee <drublee@terpmail.umd.edu>
2020-03-17 23:47:46 -0400

    gif functionality added

--- treasury_animate.R
+++ treasury_animate.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(rvest)
 library(gganimate)
+library(gifski)
 
 url <- "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yieldAll"
 
@@ -22,25 +23,32 @@ dl_fin <- dl_tbl %>%
   select(Date,Tenor,Yield,MonthAdj)
 
 
-get_date <- function(dl,curr_date){
-  dl %>%
-    filter(Date == curr_date)
-}
+# get_date <- function(dl,curr_date){
+#   dl %>%
+#     filter(Date == curr_date)
+# }
+# 
+# 
+# plot_date <- function(dl,curr_date){
+#   get_date(dl,curr_date) %>%
+#     filter(!is.na(Yield)) %>%
+#     ggplot(aes(MonthAdj,Yield)) +
+#     geom_line() + 
+#     geom_point()
+# }
 
 
-plot_date <- function(dl,curr_date){
-  get_date(dl,curr_date) %>%
-    filter(!is.na(Yield)) %>%
-    ggplot(aes(MonthAdj,Yield)) +
-    geom_line() + 
-    geom_point()
-}
-
-
+setwd(paste0(getwd(),"/gif_folder"))
 dl_fin %>%
   filter(str_extract(Date,"\\d{2}$") == "20") %>%
-  ggplot(aes(MonthAdj,Yield)) + 
+  ggplot(aes(MonthAdj,Yield,label=Tenor)) + 
   geom_line() +
   geom_point() +
+  geom_text(check_overlap = TRUE, nudge_y = 0.05) +
   labs(title = "{closest_state}") +
   transition_states(Date)
+
+gifski(list.files(),gif_file="animation.gif",delay=0.125)
+
+
+
